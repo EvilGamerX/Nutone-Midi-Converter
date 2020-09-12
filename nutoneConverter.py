a2a25ce3f2f96002c0aa7bdeb00d4cb99a0e19ad
Lilith <evilgamerx42@gmail.com>
2020-09-12 19:45:23 -0400

    JSON object for basic info
    Holding the Nutone note format in the json object and loading it

--- nutoneConverter.py
+++ nutoneConverter.py
@@ -1,9 +1,14 @@
 from mido import MidiFile
 import os
+import json
 
 def nutoneConvert(overwrite):
     if not os.path.exists('nutone'):
         os.makedirs('nutone')
         print('Nutone directory created. Please add files to that directory and try again.')
         return
-    print(f'Nutone Converter. Overwrite {overwrite}')
+    with open('nutoneNotes.json') as f:
+        conversionTable = json.load(f)
+    print(conversionTable)
+    for item in os.listdir('nutone'):
+        print(item)
