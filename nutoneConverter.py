da449790e74281065d088de1be101a2f6d008c6d
Lilith <evilgamerx42@gmail.com>
2020-09-12 21:41:16 -0400

    Created conversion directory
    Created conversion directory if it doesn't exist yet

--- nutoneConverter.py
+++ nutoneConverter.py
@@ -7,6 +7,7 @@ def nutoneConvert(overwrite):
         os.makedirs('nutone')
         print('Nutone directory created. Please add files to that directory and try again.')
         return
+    if not os.path.exists('converted/midi'):
     with open('nutoneNotes.json') as f:
         conversionTable = json.load(f)
     print(conversionTable)
