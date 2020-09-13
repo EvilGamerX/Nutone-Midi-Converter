3357183f16a0c22ed228dffa08868f7e4d9c17df
Lilith <evilgamerx42@gmail.com>
2020-09-12 21:41:53 -0400

    Writing to midi files
    Converts the text to midi files

--- nutoneConverter.py
+++ nutoneConverter.py
@@ -1,6 +1,8 @@
-from mido import MidiFile
+from mido import MidiFile, MidiTrack, Message
 import os
 import json
+
+rootNote = 48
 
 def nutoneConvert(overwrite):
     if not os.path.exists('nutone'):
@@ -8,8 +10,36 @@ def nutoneConvert(overwrite):
         print('Nutone directory created. Please add files to that directory and try again.')
         return
     if not os.path.exists('converted/midi'):
+        os.makedirs('converted/midi')
+        print('Converted midi directory created.')
     with open('nutoneNotes.json') as f:
         conversionTable = json.load(f)
-    print(conversionTable)
     for item in os.listdir('nutone'):
         print(item)
+        textFile = open(f'nutone/{item}', 'r').read().split(', ')
+        item = item.split('.')[0]
+        print(textFile)            
+        mid = MidiFile()
+        track = MidiTrack()
+        mid.tracks.append(track)
+        track.append(Message('program_change', program=12, time=0))
+
+
+        for note in textFile:
+            if(note == '00'):
+                break
+            midiNote = rootNote
+            noteLenth = 0.125
+            if(note[0] == conversionTable['octaveKey']):
+                midiNote += 12
+                note = note[1:]
+            midiNote +=  int(note[0], 12)
+            noteLenth *= int(note[1]) * 1000
+            print(f'{midiNote} | {noteLenth}\n')
+            track.append(Message('note_on', note=midiNote, velocity=64, time=int(noteLenth)))
+            track.append(Message('note_off', note=midiNote, velocity=127, time=int(noteLenth)))
+        
+        mid.save(f'converted/midi/{item}.mid')
+
+
+            
