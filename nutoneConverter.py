fea4039771467b747385f0d74e342616f460deb1
Lilith <evilgamerx42@gmail.com>
2020-09-14 12:08:09 -0400

    Nutone Speeds
    The three different speeds for the Nutone Songs

--- nutoneConverter.py
+++ nutoneConverter.py
@@ -33,6 +33,14 @@ def nutoneConvert(overwrite):
             program = 18
         track.append(Message('program_change', program=program, time=0))
 
+        speed = 1920
+        speedInput = input('Please select the speed of the Nutone song (invalid input will default to Medium):\n1) Slow\n2) Medium\n3) Fast\n')
+
+        if(speedInput == '1'):
+            speed *= 2
+        elif(speedInput == '3'):
+            speed /= 2
+
         for note in textFile:
             if(note == '00'):
                 break
@@ -42,8 +50,7 @@ def nutoneConvert(overwrite):
                 midiNote += 12
                 note = note[1:]
             midiNote +=  int(note[0], 12)
-            noteLenth *= noteMultipliers[int(note[1])-1] * 1920
-            print(f'{midiNote} | {noteLenth}\n')
+            noteLenth *= noteMultipliers[int(note[1])-1] * speed
             track.append(Message('note_on', note=midiNote, velocity=127, time=0))
             track.append(Message('note_off', note=midiNote, velocity=127, time=int(noteLenth)))
         
